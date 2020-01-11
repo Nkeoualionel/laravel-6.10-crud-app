9f5060dc8561252da090e1ca8e4afecea24ad738
unknown <nkeoualionel@gmail.com>
2020-01-11 12:05:14 +0100

    repository

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $student = Student::all();
+        return view('index', compact('student'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $storeData = $request->validate([
+            'nom' => 'required|max:255',
+            'prenom' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phone' => 'required|numeric',
+            'password' => 'required|max:255'
+        ]);
+
+        $save = Student::create($storeData);
+
+        return redirect('/students')->with('completed', 'Utilisateur créer avec succès');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $edit = Student::findOrFailed($id);
+        return view ('edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $updateData = $request->validate([
+            'nom' => 'required|max:255',
+            'prenom' => 'required|max:255',
+            'email' => 'required|max:255',
+            'phone' => 'required|numeric',
+            'password' => 'required|max:255',
+        
+        ]);
+
+            $update = Student::whereId($id)->update($updateData);
+            return redirect('/students')->with('completed', 'Utilisateur modifier avec succès');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $delete =  Student::findOrFailed($id);
+        $delete->delete();
+
+        return redirect('/students')->with('completed', 'Utilisateur effacer avec succès');
+    }
+}
